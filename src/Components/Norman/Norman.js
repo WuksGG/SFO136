67141a022d7937860b7e2a0e4683d5381837b0e1
Michael Chan <mhcchan@ucdavis.edu>
2021-06-26 14:18:01 -0700

    Updates

--- src/Components/Norman/Norman.js
+++ src/Components/Norman/Norman.js
@@ -1,4 +1,5 @@
 import React, { Component, useState } from 'react';
+import "./Norman.css";
 
 
 
@@ -7,36 +8,32 @@ export default class Norman extends Component {
   render () {
     return (
       <div className="meme">
-        <h5> Hello its me </h5>
+
         <img alt="a funky cat" src="https://media0.giphy.com/media/GeimqsH0TLDt4tScGw/giphy.gif?cid=790b761183888b5311bf659cd0429532bb03444340ce7216&rid=giphy.gif&ct=g" className="catDance"/>
-
-
+        <h2 class="tinaText"> click on tina to make it werk</h2>
+        <CheckBox />
         {/* <button onClick={() => this.setState()}> Click Meh </button> */}
       </div>
     )
   }
 }
-/*
 
-const CheckBox = ({ handleClick }) => {
+
+const CheckBox = ({ }) => {
   const [isChecked, setIsChecked] = useState(false);
-
   const toggleCheck = ()=> {
     setIsChecked(!isChecked);
-handleClick()
   }
-
-  const checkbox = isChecked ? selected : deselected;
   return (
   <div>
     <div onClick={toggleCheck}>
-    <span> <img alt={'alt'} src={checkbox} /></span>
+      <span>{ isChecked ? <img class="belcher" alt="belch1" src="https://i.imgur.com/GvUO8FO.png"/> : <img class="belcher" alt="belch2" src="https://i.imgur.com/Kc92ABL.png"/> }</span>
     </div>
   </div>
   )
 }
 
-
+/*
 
 class Belcher extends Component {
   constructor() {
